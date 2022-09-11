4f7d480274f8f392079856bb6444d003fd868c71
Marco <100785809+Marco5dev@users.noreply.github.com>
2022-09-11 23:04:22 +0200

    New Edit

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <>
       <h1 className="title">HI</h1>
-      <h2 className="title">hello</h2>
+      <h2 className="title">Home</h2>
       <h3 className="title" style={{ color: "#fff" }}>
         my name is mark
       </h3>
