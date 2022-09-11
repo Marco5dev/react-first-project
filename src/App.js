49de28a0192a89a0f1eb6c85342387ac3e4d0b27
Marco <100785809+Marco5dev@users.noreply.github.com>
2022-09-11 16:24:16 +0200

    New

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './pages/home';
+import Hello from './pages/hello'
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/hello" element={<Hello />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="title">HI</h1>
+      <h2 className="title">hello</h2>
+      <h3 className="title" style={{ color: "#fff" }}>
+        my name is mark
+      </h3>
+    </>
+  );
+};
+
+export default Home;
