6a228926526e1eb04313021a5d930b1562a802cc
Marco <100785809+Marco5dev@users.noreply.github.com>
2022-09-11 15:12:53 +0200

    New

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <>
       <h1 className="title">HI</h1>
       <h2 className="title">hello</h2>
-      <h3 className="title">my name is mark</h3>
+      <h3 className="title" style={{ color: "#fff" }}>my name is mark</h3>
     </>
   );
 }
